eb1f68ff92547df2547d840ee009299bdb9c2718
alicefesto <117024797+alicefesto@users.noreply.github.com>
2022-10-30 17:45:25 +0300

    aws labs

---
+++ basicsub.py
@@ -0,0 +1,12 @@
+def add(m,n):
+    return m+n
+def divide(m,n):
+    return m/n
+    
+num_a=20
+num_b=5
+
+add_result=add(num_a, num_b)
+print('add:{}+{}={}',formart(num_a, num_b, add_result))
+divide_result=divide(num_a, num_b)
+prind('divide:{}/{}={}')
---
+++ mymodule.py
@@ -0,0 +1,5 @@
+import math
+
+
+def calculate_circle_area(radius):
+    return math.pi*radius**2
